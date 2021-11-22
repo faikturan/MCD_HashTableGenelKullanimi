87879bb779209f05a0cba74117eaa19d0b66369e
Faik Turan <turanfaik@hotmail.com>
2021-11-22 20:35:33 +0300

    changed

--- MCD_HashTableGenelKullanimi/Program.cs
+++ MCD_HashTableGenelKullanimi/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace MCD_HashTableGenelKullanimi
 {
@@ -10,7 +11,56 @@ namespace MCD_HashTableGenelKullanimi
     {
         static void Main(string[] args)
         {
+            Hashtable H1 = new Hashtable();
 
+            #region Yeni değer ekleme
+            H1.Add("Car", "Araba");
+            H1.Add("House", "Ev");
+
+            H1.Add("Cars", "Araba");
+            //H1.Add("Cars", "Arabalar");Key değeri benzersiz olmak zorundadır.
+            foreach (var item in H1.Values)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+
+            #region Yardımcı Metodlar
+            bool Kontrol1 = H1.Contains("House");
+            bool Kontrol2 = H1.Contains("Door");
+            Console.WriteLine(Kontrol1);
+            Console.WriteLine(Kontrol2);
+
+            bool Kontrol3 = H1.ContainsKey("House");
+            bool Kontrol4 = H1.ContainsValue("Araba");
+            Console.WriteLine(Kontrol3);
+            Console.WriteLine(Kontrol4);
+
+            foreach (var item in H1.Keys)
+            {
+                Console.WriteLine(H1[item]+" "+item);
+            }
+
+            H1["House"] = "Villa";
+
+            H1.Remove("Cars");
+
+            Console.WriteLine("***************");
+            foreach (var item in H1.Keys)
+            {
+                Console.WriteLine(H1[item] + " " + item);
+            }
+
+            int koleksiyonIcindekiToplamDeger = H1.Count;
+            Console.WriteLine(koleksiyonIcindekiToplamDeger);
+            H1.Clear();//Koleksiyon içerisindeki tüm datayı temizleriz.
+            int koleksiyonIcindekiToplamDeger2 = H1.Count;
+            Console.WriteLine(koleksiyonIcindekiToplamDeger2);
+
+
+            #endregion
+
+            Console.ReadLine();
         }
     }
 }
